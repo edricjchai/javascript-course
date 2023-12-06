853ef0d4faeb580bf04d254d297b76845a09068e
Edric Chai <edricchai@gmail.com>
2023-12-05 22:49:55 -0500

    12/5/2023 Advanced Functions Part 2

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -11,22 +11,32 @@ renderTodoList();
 function renderTodoList(){
   let todoListHTML = '';
 
-  todoList.forEach(function(todoObject, index) {
+  todoList.forEach((todoObject, index) => {
     const { name, dueDate }  = todoObject;
 
     //generating the html
     const html = `
       <div>${name}</div>
       <div>${dueDate}</div>
-      <button onclick="
-        todoList.splice(${index}, 1);
-        renderTodoList();
-      " class = "delete-todo-button">Delete</button>`
+      <button class = "delete-todo-button js-delete-todo-button">Delete</button>`;
     todoListHTML += html;
   });
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
 }
+
+document.querySelector('.js-add-todo-button')
+  .addEventListener('click', () => {
+    addToDo();
+  })
 
 function addToDo(){
   const inputElement = document.querySelector('.js-name-input');
